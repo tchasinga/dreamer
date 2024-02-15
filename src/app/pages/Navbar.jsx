bdf6d7c9221d3298d6ea630811060d49048c96af
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-15 10:06:13 +0200

    new code is added now

--- src/app/pages/Navbar.jsx
+++ src/app/pages/Navbar.jsx
@@ -7,9 +7,16 @@ export default function Navbar() {
         <div className="container max-w-4xl mx-auto p-5 text-white">
             {/* Logo section */}
             <div className="">
-                <Link href="/" className="text-2xl font-bold">Dreamer</Link>
+                <Link href="/" className="text-2xl md:text-2xl  font-medium">Dreamer</Link>
             </div>
             {/* Desktop section */}
+             <div className="">
+                <ul>
+                    <li className="inline-block px-5">Home</li>
+                    <li className="inline-block px-5">Service</li>
+                    <li className="inline-block px-5">Contact</li>
+                </ul>
+             </div>
             {/* Mobile section */}
         </div>
      </header>
