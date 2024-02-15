6889c8de9ce8f4fd9358fff12e079718bd46a657
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-15 09:59:44 +0200

    new code is added now

--- src/app/pages/Navbar.jsx
+++ src/app/pages/Navbar.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default function Navbar() {
   return (
      <header className="bg-blue-700">
-        <div className="container max-w-4xl mx-auto p-5">
-            <h1 className="">Balolebwami</h1>
+        <div className="container max-w-4xl mx-auto p-5 text-white">
+            {/* Logo section */}
+            {/* Desktop section */}
+            {/* Mobile section */}
         </div>
      </header>
   )
