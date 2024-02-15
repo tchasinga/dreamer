a3f10edb5bdadf24ef012f8f61df403f009792f1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-15 09:50:11 +0200

    new code is added now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Navbar from "./pages/Navbar";
 
 export default function Home() {
   return (
     <main className=''>
+      <Navbar />
     </main>
   );
 }
---
+++ src/app/pages/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+     <header className="bg-blue-700">
+        <div className="container">
+            <p>Balolebwami</p>
+        </div>
+     </header>
+  )
+}
