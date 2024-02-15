17835885c27b6d2e8bac0a7833b7f9353082beb5
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-15 10:01:42 +0200

    new code is added now

--- src/app/pages/Navbar.jsx
+++ src/app/pages/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
      <header className="bg-blue-700">
         <div className="container max-w-4xl mx-auto p-5 text-white">
             {/* Logo section */}
+            <div className="">
+                <Link href="/" className="text-2xl font-bold">Dreamer</Link>
+            </div>
             {/* Desktop section */}
             {/* Mobile section */}
         </div>
