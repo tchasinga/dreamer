39e2aeb5aa481d580fe75ca38865be2fa29be17f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-15 10:09:05 +0200

    new code is added now

--- src/app/pages/Navbar.jsx
+++ src/app/pages/Navbar.jsx
@@ -4,17 +4,17 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
      <header className="bg-blue-700">
-        <div className="container max-w-4xl mx-auto p-5 text-white">
+        <div className="container max-w-4xl mx-auto p-5 text-white flex justify-between items-center">
             {/* Logo section */}
             <div className="">
                 <Link href="/" className="text-2xl md:text-2xl  font-medium">Dreamer</Link>
             </div>
             {/* Desktop section */}
              <div className="">
-                <ul>
-                    <li className="inline-block px-5">Home</li>
-                    <li className="inline-block px-5">Service</li>
-                    <li className="inline-block px-5">Contact</li>
+                <ul className="flex items-center justify-center gap-3">
+                    <li className="">Home</li>
+                    <li className="">Service</li>
+                    <li className="">Contact</li>
                 </ul>
              </div>
             {/* Mobile section */}
